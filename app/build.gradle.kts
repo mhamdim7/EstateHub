1ea2fe24dc2f14e917d514fa62b0db72d5b55c70
Sameh <mhamdiapk@gmail.com>
2024-02-02 14:27:50 +0100

    Enhance ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,15 +55,12 @@ dependencies {
 
     // Default
     implementation("androidx.core:core-ktx:1.12.0")
-    // lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     // Compose
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     // Material
     implementation("androidx.compose.material3:material3")
     // Navigation
@@ -93,6 +90,5 @@ dependencies {
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation("android.arch.core:core-testing:1.1.1")
     testImplementation ("io.mockk:mockk:1.13.5")
-
 
 }
--- app/src/main/java/com/sameh/estatehub/presentation/ui/components/EstatesScreenComponents.kt
+++ app/src/main/java/com/sameh/estatehub/presentation/ui/components/EstatesScreenComponents.kt
@@ -1,11 +1,14 @@
 package com.sameh.estatehub.presentation.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -18,12 +21,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.constraintlayout.compose.ConstrainedLayoutReference
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
 import com.sameh.estatehub.R
 import com.sameh.estatehub.presentation.ui.EstateDetailsUiModel
@@ -32,7 +38,7 @@ import com.sameh.estatehub.presentation.ui.theme.dimens
 
 @Composable
 fun EstateList(items: List<EstateDetailsUiModel>, onUserClicked: (listingId: String) -> Unit) {
-    LazyColumn {
+    LazyColumn(contentPadding = PaddingValues(MaterialTheme.dimens.medium)) {
         items(
             count = items.size,
             key = { items[it].id },
@@ -40,7 +46,6 @@ fun EstateList(items: List<EstateDetailsUiModel>, onUserClicked: (listingId: Str
         )
     }
 }
-
 
 @Composable
 private fun EstateItem(
@@ -50,48 +55,87 @@ private fun EstateItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(MaterialTheme.dimens.small)
+            .wrapContentHeight()
+            .padding(vertical = MaterialTheme.dimens.small)
             .clickable { onUserClicked(estate.id.toString()) },
         shape = MaterialTheme.shapes.medium
     ) {
-        Column(
+        ConstraintLayout(
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
         ) {
+            val image = ConstrainedLayoutReference(stringResource(R.string.image))
+            val column = ConstrainedLayoutReference(stringResource(R.string.column))
+            val overlayColor = if (isSystemInDarkTheme()) Color.Black else Color.White
             AsyncImage(
+                modifier = Modifier
+                    .constrainAs(image) {
+                        top.linkTo(column.top)
+                        bottom.linkTo(column.bottom)
+                        start.linkTo(column.start)
+                        end.linkTo(column.end)
+                        width = Dimension.matchParent
+                        height = Dimension.fillToConstraints
+                    },
                 placeholder = rememberVectorPainter(image = Icons.Outlined.Warning),
-                error = painterResource(id = R.drawable.ic_launcher_foreground),
+                error = painterResource(id = R.drawable.ic_image_error),
                 model = estate.url,
                 contentDescription = stringResource(id = R.string.image_content_description),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(MaterialTheme.dimens.estateItemImageHeight)
-                    .clip(shape = MaterialTheme.shapes.medium),
                 contentScale = ContentScale.Crop
             )
-            Spacer(modifier = Modifier.height(MaterialTheme.dimens.small))
-            EstateEntryItem(title = stringResource(R.string.city), value = estate.city)
-            EstateEntryItem(title = stringResource(R.string.property_type), value = estate.propertyType)
-            estate.bedrooms?.let {
-                EstateEntryItem(title = stringResource(R.string.bedrooms), value = estate.bedrooms.toString())
-            }
-            EstateEntryItem(title = stringResource(R.string.professional), value = estate.professional)
-            EstateEntryItem(title = stringResource(R.string.area), value = stringResource(R.string.sqm, estate.area))
-            EstateEntryItem(title = stringResource(R.string.price), value = estate.price.toString())
-            estate.rooms?.let {
-                EstateEntryItem(title = stringResource(R.string.rooms), value = estate.rooms.toString())
+            Column(
+                modifier = Modifier
+                    .constrainAs(column) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+                    .fillMaxSize()
+                    .background(overlayColor.copy(alpha = 0.4f))
+            )
+            {
+                EstateEntryItem(
+                    title = stringResource(R.string.area),
+                    value = stringResource(R.string.sqm, estate.area)
+                )
+                EstateEntryItem(
+                    title = stringResource(R.string.price),
+                    value = estate.price.toString()
+                )
+                estate.rooms?.let {
+                    EstateEntryItem(
+                        title = stringResource(R.string.rooms),
+                        value = estate.rooms.toString()
+                    )
+                }
+                EstateEntryItem(title = stringResource(R.string.city), value = estate.city)
+                EstateEntryItem(
+                    title = stringResource(R.string.property_type),
+                    value = estate.propertyType
+                )
+                estate.bedrooms?.let {
+                    EstateEntryItem(
+                        title = stringResource(R.string.bedrooms),
+                        value = estate.bedrooms.toString()
+                    )
+                }
+                EstateEntryItem(
+                    title = stringResource(R.string.professional),
+                    value = estate.professional
+                )
             }
         }
     }
 }
 
 @Composable
- private fun EstateEntryItem(title: String, value: String) {
+private fun EstateEntryItem(title: String, value: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(MaterialTheme.dimens.small),
+            .padding(MaterialTheme.dimens.verySmall),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -99,10 +143,10 @@ private fun EstateItem(
             style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
             modifier = Modifier.weight(1f)
         )
-        Spacer(modifier = Modifier.width(MaterialTheme.dimens.small))
+        Spacer(modifier = Modifier.width(MaterialTheme.dimens.verySmall))
         Text(
             text = value,
-            style = MaterialTheme.typography.bodySmall,
+            style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.weight(1f)
         )
     }
